b692cd51590d9f3a45a54f9f6c1e55c65d7801ee
Victor Grau Serrat <lacabra@users.noreply.github.com>
2023-03-04 19:16:57 -0700

    BLD: add admin utilities

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,5 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
+
 import { orgs } from "../components/orgs"
+import MyHead from "../components/head"
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '@/styles/Home.module.css'
+
 
 export default function Admin() {
 
@@ -8,25 +19,58 @@ export default function Admin() {
     const percentage = 10;
     const charity = 1;
 
+    useEffect(() => {
+        const d = JSON.parse(window.localStorage.getItem("donations"))
+        setDonations(d);
+    }, [])
+
+    // const donation = {
+    //     percentage: percentage,
+    //     orgName: orgs[charity].name,
+    //     orgAddress: orgs[charity].address,
+    //     splitContract: '0xcBBfC0F4C8EF45aBd1704d737F64d302587436d1'
+    // }
+
     const donation = {
-        percentage: percentage,
-        orgName: orgs[charity - 1].name,
-        orgAddress: orgs[charity - 1].address,
-        splitContract: '0xcBBfC0F4C8EF45aBd1704d737F64d302587436d1'
+        percentage: 15,
+        orgName: 'Save the Children',
+        orgAddress: '0x98fE6b33eb2ecf7875B3010879bFEA9d6c4CACEC',
+        splitContract: '0x226736C7D26c914d7c4dbb6964e9a52C4Ad6004b'
     }
 
-    useEffect(() => {
+    function addEntry() {
         var d = JSON.parse(window.localStorage.getItem("donations"))
-        if (typeof d !== 'undefined') {
+        console.log(d)
+        if (typeof d === 'undefined') {
             d = []
         }
+        console.log(d)
         d.push(donation)
+        console.log(d)
 
         window.localStorage.setItem("donations", JSON.stringify(d));
         setDonations(d)
-    }, []);
+    }
+
+    function removeLast() {
+        var d = JSON.parse(window.localStorage.getItem("donations"))
+        if (typeof d !== 'undefined') {
+            d.pop();
+        }
+        window.localStorage.setItem("donations", JSON.stringify(d));
+        setDonations(d)
+    }
         
     return (
-        <p>{JSON.stringify(donations)}</p>
+        <>
+            <MyHead />
+            <main className={styles.main}>
+                <Container>
+                    <Row>{JSON.stringify(donations, null, " ")}</Row>
+                    <Button onClick={addEntry}>Add entry</Button>&nbsp;&nbsp;
+                    <Button onClick={removeLast}>Remove Last</Button>
+                </Container>
+            </main>
+        </>
     )
 }
